4b243060a096dfced955de8b25ae99514ac1963c
Rouzbeh Rouzbahani <rrouzbahani2@students.wcpss.net>
2019-10-23 21:41:42 -0400

    as

---
+++ BattleBots/Player.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleBots
+{
+    class Player
+    {
+        private string name;
+        private int score;
+        private int highestscore;
+        private string weapon;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Weapon
+        {
+            get
+            {
+                return weapon;
+            }
+            set
+            {
+                weapon = value;
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                score = value;
+            }
+        }
+        public int HighestScore
+        {
+            get
+            {
+                return highestscore;
+            }
+            set
+            {
+                highestscore = value;
+            }
+        }
+        public Player()
+        {
+            Name = "Player";
+            Weapon = "Sledge Hammer";
+        }
+        public Player(string name, string weapon)
+        {
+            Name = name;
+            Weapon = weapon;
+        }
+        public void Scoring()
+        {
+            for(int i = 0; i < 1; i++)
+            {
+                HighestScore = Score;
+
+            }
+            if (Score > HighestScore)
+                HighestScore = Score;
+        }
+    }
+}
---
+++ BattleBots/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleBots
+{
+    class Vehicle : Player
+    {
+        public int Fuel {get;set;}
+        public int HP {get;set;}
+        public int Points {get;set;}
+
+        public void FuelDown()
+        {
+            Fuel--;
+        }
+        public void HPDown()
+        {
+            HP--;
+        }
+        public void PointsUp()
+        {
+            Points++;
+        }
+        public void HPUp()
+        {
+            HP++;
+        }
+        public void FuelUp()
+        {
+            Fuel++;
+        }
+        abstract 
+    }
+}
