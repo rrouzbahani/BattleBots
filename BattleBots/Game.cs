e86247d4211e2f6de97a02e502b09aa57249e572
Rouzbeh Rouzbahani <rrouzbahani2@students.wcpss.net>
2019-10-24 08:46:27 -0400

    as

--- BattleBots/Game.cs
+++ BattleBots/Game.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BattleBots
 {
     class Game
     {
+        Timer timer;
+        public int TimeStartGame;
+        public int TimeStartBattle;
+        public int TimeElapsed;
+        public void game()
+        {
+            timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+        }
 
-
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeStartGame++;
+            TimeElapsed = TimeStartGame - TimeStartBattle;
+        }
     }
 }
